b5175db96f771b88dbd3d04cb8937c83404da584
sufailbadushat <sufailbadushat@gamil.com>
2023-07-13 19:13:21 +0530

    order service request complete

--- src/main/java/com/shopping/orderservice/controller/OrderController.java
+++ src/main/java/com/shopping/orderservice/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.shopping.orderservice.controller;
 
 import com.shopping.orderservice.dto.OrderRequest;
+import com.shopping.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/order")
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
         return "Order Placed Successfully...!!";
     }
 }
--- src/main/java/com/shopping/orderservice/service/OrderService.java
+++ src/main/java/com/shopping/orderservice/service/OrderService.java
@@ -6,15 +6,19 @@ import com.shopping.orderservice.dto.OrderRequest;
 import com.shopping.orderservice.model.Order;
 import com.shopping.orderservice.model.OrderLineItems;
 import com.shopping.orderservice.repository.OrderRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
-@RequiredArgsConstructor
+
 @Service
+@Transactional
 public class OrderService {
 
+    @Autowired
     private OrderRepository orderRepository;
 
     public void placeOrder(OrderRequest orderRequest){
@@ -37,6 +41,7 @@ public class OrderService {
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
         return orderLineItems;
     }
 }
